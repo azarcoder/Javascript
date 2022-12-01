78fc26504e0efdbaa9a523d900d33ed536e79642
Azarudeen <76832585+azarcoder@users.noreply.github.com>
2022-12-01 20:09:51 +0530

    Add files via upload

---
+++ Basics/Variable/localvar.js
@@ -0,0 +1,7 @@
+var a=30
+function hi(){
+    let a=10
+    console.log(a)
+}
+console.log(a)
+hi()
---
+++ Basics/Variable/typecon.js
@@ -0,0 +1,14 @@
+/*1.implicit-automate by JS
+2.explicit- by devoloper*/
+// var a=10
+// var t='a'
+// console.log(typeof(a+t))
+// let d='azar'
+// var b='deen'
+// console.log(d+b)
+
+//explicit
+console.log(Number('12'*2)=='24')
+console.log(typeof(56==String('56')))
+console.log(Boolean(' '))//empty - false but if any space are there consider as true
+console.log(typeof(undefined)) //undefined
